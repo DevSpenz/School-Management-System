788ae8829d4d04cd93293d2521ce6b93c9578894
DevSpenz <spenzluis@gmail.com>
2026-01-16 09:04:04 +0300

    part 1 complete frontend home dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -3,6 +3,8 @@ import UserCard from "@/components/UserCard";
 import CountChart from "@/components/CountChart";
 import AttendanceChart from "@/components/AttendanceChart";
 import FinanceChart from "@/components/FinanceChart";
+import EventCalendar from '@/components/EventCalendar';
+import Announcements from '@/components/Announcements';
 
 const AdminPage = () => {
     return (
@@ -35,7 +37,10 @@ const AdminPage = () => {
                 </div>
 
             {/* Right */}
-                <div className="w-full lg:w-1/3">R</div>
+                <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                    <EventCalendar />
+                    <Announcements />
+                </div>
             </div>
     )
 }
