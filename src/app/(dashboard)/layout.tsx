8af4bb02c4d03de8aa7e70f9ad578739b41dc15c
DevSpenz <spenzluis@gmail.com>
2026-01-16 12:01:44 +0300

    student page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ function DashboardLayout({children}: {children: React.ReactNode}) {
     return (
         <div className={"h-screen flex"}>
         {/*Left*/}
-            <div className={"w-[16%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-slate-100 px-2"}>
+            <div className={"w-[16%] md:w-[8%] lg:w-[16%] xl:w-[14%] px-2"}>
                 <Link href={"/"} className={"flex items-center lg:justify-start justify-center gap-2 p-4"}>
                     <Image src={"/logo.png"} alt={"logo"} width={32} height={32} />
                     <span className={"hidden lg:block"}>SchooLama</span>
